33457ed18dcbcac9f28e5bc166fa00b892130812
Pablo Torrez <pablotorrez90@outlook.com>
2024-08-23 00:33:47 -0300

    agregando contenedores grid en dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -79,9 +79,17 @@ function App() {
             {/* <HeaderDashboard /> */}
               <PageDashboard /> 
             <main className='flex-grow p-4 ml-64'>
-              <div className="artboard artboard-horizontal phone-1 bg-black">568×320</div>
+              {/* <div className="artboard artboard-horizontal phone-1 bg-black">568×320</div>
               <div className="artboard artboard-horizontal phone-3 bg-blue-700">736×414</div>
-              <div className="artboard phone-1 bg-red-600">320×568</div>
+              <div className="artboard phone-1 bg-red-600">320×568</div> */}
+              <div className="grid grid-cols-6 grid-rows-6 gap-4">
+                <div className='col-span-2 row-span-2 rounded-3xl bg-slate-400 px-5 py-20'>01 - Numero de tarjeta</div>
+                <div className='col-span-2 row-span-2 rounded-3xl bg-slate-400 px-5 py-10'>02 - Wallet (income - expenses)</div>
+                <div className='col-span-2 row-span-3 rounded-3xl bg-slate-400 px-5 py-52'>03 - Transactions</div>
+                <div className='col-span-2 row-span-4 rounded-3xl bg-slate-400 px-5 py-10'>04 - Monthly earnings (income)</div>
+                <div className='col-span-2 row-span-2 rounded-3xl bg-slate-400 px-5 py-10'>05 - Earnings (goals, analytics graphics)</div>
+                <div className='col-span-2 row-span-2 rounded-3xl bg-slate-400 px-5 py-10'>06 - Payable Accounts (receipts(salary, service, rent or mortgage), payables(electricity Bill, rent or mortgage))</div>
+              </div>
             </main>
             {/* <FooterDashboard /> */}
           </div>
--- src/pages/PageDashboard.jsx
+++ src/pages/PageDashboard.jsx
@@ -45,7 +45,7 @@ export default function PageDashboard() {
   }, []);
 
   return (
-    <div className="drawer lg:drawer-open">
+    <div className="drawer lg:drawer-open w-0">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col items-center justify-center">
         <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">
